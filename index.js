087ffe34e1b0cb6d235bbd6701eee74a7645ba4e
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2024-12-20 18:58:50 +0100

    Add task

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-let first = "Hello , Google";
-console.log(first);
+const a = 10;
+const b = 15;
+console.log(a * b);
+
+const c = 10;
+const d = 5;
+console.log(c / d);
+
+const e = 20;
+const f = 2;
+console.log(e + f);
+
+const someNumber = 11;
+const boolConst = true;
+const stringConst = "java script";
+const numberInString = "100";
+console.log(someNumber, boolConst, stringConst, numberInString);
+
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++
+num--
