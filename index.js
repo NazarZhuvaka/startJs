6c231bd675e9f018a1d72fbfc59f3a674c06abcc
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-01-23 11:05:09 +0100

    End

--- index.js
+++ index.js
@@ -1,68 +1,141 @@
-// const a = 10;
-// const b = 15;
-// console.log(a * b);
+// --------- Task 1 -----------
 
-// const c = 10;
-// const d = 5;
-// console.log(c / d);
+const firstNum = Number(prompt("Введіть перше число"));
+const secondNum = Number(prompt("Введіть друге число"));
 
-// const e = 20;
-// const f = 2;
-// console.log(e + f);
+if ((firstNum + secondNum) % 5 === 0) {
+  alert("Сума кратна 5");
+} else {
+  alert("Сума НЕ кратна 5");
+}
 
-// const someNumber = 11;
-// const boolConst = true;
-// const stringConst = "java script";
-// const numberInString = "100";
-// console.log(someNumber, boolConst, stringConst, numberInString);
+// --------- Task 2 ------------
 
-// let num = 1;
-// num += 11;
-// num -= 11;
-// num *= 11;
-// num /= 11;
-// num++;
-// num--;
+function squareArea(side) {
+  return Math.pow(side, 2);
+}
 
-// const userNumber = Number(prompt("Введіть число:"));
-// alert(Math.pow(userNumber, 2));
+console.log(squareArea(2));
 
-// const firstNumber = Number(prompt("Введіть перше число:"));
-// const secondNumber = Number(prompt("Введіть друге число:"));
-// alert((firstNumber + secondNumber) / 2);
+// --------- Task 3 ------------
 
-// const userTime = Number(prompt("Введіть кількість хвилин:"));
-// alert(userTime * 60);
+const userNumber = Number(prompt("Введіть число "));
 
-// const greeting = "Hello, ";
-// let userName;
-// userName = prompt("Вкажіть Ваше ім'я:");
-// alert(greeting + userName + "!");
-// 3;
+switch (userNumber) {
+  case 1: {
+    console.log("Ви обрали сік");
+    break;
+  }
+  case 2: {
+    console.log("Ви обрали воду");
+    break;
+  }
+  case 3: {
+    console.log("Ви обрали каву");
+    break;
+  }
+  case 4: {
+    console.log("Ви обрали чай");
+    break;
+  }
+  case 5: {
+    console.log("Ви обрали лимонад");
+    break;
+  }
 
-// if (true or false) {
-    // true
-// } else {
-    // false
-// }
+  default: {
+    console.log("Такого напою у нас нема");
+  }
+}
 
-// const isLogged = true
+// --------- Task 3.1 (калькулятор) ------------
 
-// if (isLogged) {
-//     // true
-//     console.log("Yes, user is logged")
-// } else {
-//     // false
-//     console.log("User is not logged")
-// }
+function culculate() {
+  const firstNum = Number(prompt("Введіть перше число "));
+  const secondNum = Number(prompt("Введіть друге число "));
+  const operation = prompt("Введіть дію ");
 
-// const firstNumber = Number(prompt("Перше число:"))
-// const userOperation = prompt("Введіть операцію:")
-// const secondNumber = Number(prompt("Друге число:"))
+  switch (operation) {
+    case "+": {
+      return plusNumber(firstNum, secondNum);
+    }
+    case "-": {
+      return minusNumber(firstNum, secondNum);
+    }
+    case "*": {
+      return insemNumber(firstNum, secondNum);
+    }
+    case "/": {
+      return divisNumber(firstNum, secondNum);
+    }
 
-// if (userOperation === "+") {
-//     alert(firstNumber + secondNumber)
-// } else {
-//     alert("Операцію вмконати неможливо")
-// }
+    default: {
+      console.log("Error");
+    }
+  }
+}
 
+console.log(culculate());
+
+function plusNumber(num1, num2) {
+  return num1 + num2;
+}
+
+function minusNumber(num1, num2) {
+  return num1 - num2;
+}
+
+function insemNumber(num1, num2) {
+  return num1 * num2;
+}
+
+function divisNumber(num1, num2) {
+  return num1 / num2;
+}
+
+// --------- Task 4 ------------
+
+const sum = Number(prompt("Введіть суму покупки "));
+
+function calcSum(param1) {
+  const endSum =
+    param1 > 800
+      ? param1 * 1.05
+      : param1 > 500
+      ? param1 * 1.03
+      : "Менше суми знижки";
+
+  return endSum;
+}
+
+console.log(calcSum(sum));
+
+// --------- Task 5 ------------
+
+function dropFunc(num1, num2) {
+  const endNum = num1 % num2 === 0 ? true : false;
+
+  return endNum;
+}
+
+console.log(dropFunc(5, 5));
+
+// --------- Task 6* ------------
+
+function solveQuadratic(num1, num2, num3) {
+  const D = Math.pow(num2, 2) - 4 * num1 * num3;
+
+  if (D > 0) {
+    let result = ((-num2 + Math.sqrt(D)) / 2) * num1;
+    console.log(result);
+    result = ((-num2 - Math.sqrt(D)) / 2) * num1;
+    console.log(result);
+  } else if (D === 0) {
+    let result = (-num2 / 2) * num1;
+    console.log(result);
+  } else {
+    console.log("Розв'язків немає!");
+  }
+}
+
+console.log(solveQuadratic(1, -3, 2));
