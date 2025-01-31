331dc2ce18e3e251e27f11d4648a030fe4c7f4fe
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-01-31 15:13:10 +0100

    Add loops

--- index.js
+++ index.js
@@ -1,141 +1,91 @@
-// // --------- Task 1 -----------
+// --------- Task 1 -----------
 
-// const firstNum = Number(prompt("Введіть перше число"));
-// const secondNum = Number(prompt("Введіть друге число"));
+for (let i = 1; i < 10; i++) {
+  let result = i ** 2;
+  console.log(result);
+}
 
-// if ((firstNum + secondNum) % 5 === 0) {
-//   alert("Сума кратна 5");
-// } else {
-//   alert("Сума НЕ кратна 5");
-// }
+// --------- Task 2 -----------
 
-// // --------- Task 2 ------------
+function isCorrect() {
+  const correctPass = "qwerty1234";
 
-// function squareArea(side) {
-//   return Math.pow(side, 2);
-// }
+  while (true) {
+    const userPass = prompt("Enter your password: ");
 
-// console.log(squareArea(2));
+    if (userPass === correctPass){
+        console.log("Ви успішно увійшли в систему !");
+        break
+    }
+  }
+}
 
-// // --------- Task 3 ------------
+isCorrect();
 
-// const userNumber = Number(prompt("Введіть число "));
+// --------- Task 3 -----------
 
-// switch (userNumber) {
-//   case 1: {
-//     console.log("Ви обрали сік");
-//     break;
-//   }
-//   case 2: {
-//     console.log("Ви обрали воду");
-//     break;
-//   }
-//   case 3: {
-//     console.log("Ви обрали каву");
-//     break;
-//   }
-//   case 4: {
-//     console.log("Ви обрали чай");
-//     break;
-//   }
-//   case 5: {
-//     console.log("Ви обрали лимонад");
-//     break;
-//   }
+let result = 0;
 
-//   default: {
-//     console.log("Такого напою у нас нема");
-//   }
-// }
+for (let i = 1; i < 100; i++) {
+    result = result + i;
+    console.log(result);
+}
 
-// // --------- Task 3.1 (калькулятор) ------------
+// --------- Task 4 -----------
 
-// function culculate() {
-//   const firstNum = Number(prompt("Введіть перше число "));
-//   const secondNum = Number(prompt("Введіть друге число "));
-//   const operation = prompt("Введіть дію ");
+for(let i = 10; i < 50; i++) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
 
-//   switch (operation) {
-//     case "+": {
-//       return plusNumber(firstNum, secondNum);
-//     }
-//     case "-": {
-//       return minusNumber(firstNum, secondNum);
-//     }
-//     case "*": {
-//       return insemNumber(firstNum, secondNum);
-//     }
-//     case "/": {
-//       return divisNumber(firstNum, secondNum);
-//     }
+// --------- Task 5 -----------
 
-//     default: {
-//       console.log("Error");
-//     }
-//   }
-// }
+function doTriangle(size) {
+  let star = "";
 
-// console.log(culculate());
+  for (let i = 0; i < size; i++) {
+    star += "*";
+    console.log(star);
+  }
+}
 
-// function plusNumber(num1, num2) {
-//   return num1 + num2;
-// }
+doTriangle(7);
 
-// function minusNumber(num1, num2) {
-//   return num1 - num2;
-// }
+// --------- Task 6 -----------
 
-// function insemNumber(num1, num2) {
-//   return num1 * num2;
-// }
+function doSquare(size) {
+  for (let i = 0; i < size; i++) {
+    let row = "";
 
-// function divisNumber(num1, num2) {
-//   return num1 / num2;
-// }
+    for (let j = 0; j < size; j++) {
+      if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
+        row += "*";
+      } else {
+        row += " "; 
+      }
+    }
+    console.log(row);
+  }
+}
 
-// // --------- Task 4 ------------
+doSquare(16);
 
-// const sum = Number(prompt("Введіть суму покупки "));
+// --------- Task 7* -----------
 
-// function calcSum(param1) {
-//   const endSum =
-//     param1 > 800
-//       ? param1 * 1.05
-//       : param1 > 500
-//       ? param1 * 1.03
-//       : "Менше суми знижки";
+function doSquare(size) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
 
-//   return endSum;
-// }
+    for (let j = 0; j < size; j++) {
+        if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i + j === size - 1) {
+            row += '*';
+        } else {
+            row += ' ';
+        }
+    }
+    console.log(row);
+}
+}
 
-// console.log(calcSum(sum));
-
-// // --------- Task 5 ------------
-
-// function dropFunc(num1, num2) {
-//   const endNum = num1 % num2 === 0 ? true : false;
-
-//   return endNum;
-// }
-
-// console.log(dropFunc(5, 5));
-
-// // --------- Task 6* ------------
-
-// function solveQuadratic(num1, num2, num3) {
-//   const D = Math.pow(num2, 2) - 4 * num1 * num3;
-
-//   if (D > 0) {
-//     let result = ((-num2 + Math.sqrt(D)) / 2) * num1;
-//     console.log(result);
-//     result = ((-num2 - Math.sqrt(D)) / 2) * num1;
-//     console.log(result);
-//   } else if (D === 0) {
-//     let result = (-num2 / 2) * num1;
-//     console.log(result);
-//   } else {
-//     console.log("Розв'язків немає !");
-//   }
-// }
-
-// console.log(solveQuadratic(1, -3, 2));
+doSquare(16);
