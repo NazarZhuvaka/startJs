42ddf066711e4ec72f85a94c61ed9a5f7b366e54
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-02-01 10:53:00 +0100

    Add book shop

--- index.js
+++ index.js
@@ -1,91 +1,91 @@
-// --------- Task 1 -----------
+// // --------- Task 1 -----------
 
-for (let i = 1; i < 10; i++) {
-  let result = i ** 2;
-  console.log(result);
-}
+// for (let i = 1; i < 10; i++) {
+//   let result = i ** 2;
+//   console.log(result);
+// }
 
-// --------- Task 2 -----------
+// // --------- Task 2 -----------
 
-function isCorrect() {
-  const correctPass = "qwerty1234";
+// function isCorrect() {
+//   const correctPass = "qwerty1234";
 
-  while (true) {
-    const userPass = prompt("Enter your password: ");
+//   while (true) {
+//     const userPass = prompt("Enter your password: ");
 
-    if (userPass === correctPass){
-        console.log("Ви успішно увійшли в систему !");
-        break
-    }
-  }
-}
+//     if (userPass === correctPass){
+//         console.log("Ви успішно увійшли в систему !");
+//         break
+//     }
+//   }
+// }
 
-isCorrect();
+// isCorrect();
 
-// --------- Task 3 -----------
+// // --------- Task 3 -----------
 
-let result = 0;
+// let result = 0;
 
-for (let i = 1; i < 100; i++) {
-    result = result + i;
-    console.log(result);
-}
+// for (let i = 1; i < 100; i++) {
+//     result = result + i;
+//     console.log(result);
+// }
 
-// --------- Task 4 -----------
+// // --------- Task 4 -----------
 
-for(let i = 10; i < 50; i++) {
-    if (i % 5 === 0) {
-        console.log(i);
-    }
-}
+// for(let i = 10; i < 50; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
 
-// --------- Task 5 -----------
+// // --------- Task 5 -----------
 
-function doTriangle(size) {
-  let star = "";
+// function doTriangle(size) {
+//   let star = "";
 
-  for (let i = 0; i < size; i++) {
-    star += "*";
-    console.log(star);
-  }
-}
+//   for (let i = 0; i < size; i++) {
+//     star += "*";
+//     console.log(star);
+//   }
+// }
 
-doTriangle(7);
+// doTriangle(7);
 
-// --------- Task 6 -----------
+// // --------- Task 6 -----------
 
-function doSquare(size) {
-  for (let i = 0; i < size; i++) {
-    let row = "";
+// function doSquare(size) {
+//   for (let i = 0; i < size; i++) {
+//     let row = "";
 
-    for (let j = 0; j < size; j++) {
-      if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
-        row += "*";
-      } else {
-        row += " "; 
-      }
-    }
-    console.log(row);
-  }
-}
+//     for (let j = 0; j < size; j++) {
+//       if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
+//         row += "*";
+//       } else {
+//         row += " "; 
+//       }
+//     }
+//     console.log(row);
+//   }
+// }
 
-doSquare(16);
+// doSquare(16);
 
-// --------- Task 7* -----------
+// // --------- Task 7* -----------
 
-function doSquare(size) {
-  for (let i = 0; i < size; i++) {
-    let row = '';
+// function doSquare(size) {
+//   for (let i = 0; i < size; i++) {
+//     let row = '';
 
-    for (let j = 0; j < size; j++) {
-        if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i + j === size - 1) {
-            row += '*';
-        } else {
-            row += ' ';
-        }
-    }
-    console.log(row);
-}
-}
+//     for (let j = 0; j < size; j++) {
+//         if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i + j === size - 1) {
+//             row += '*';
+//         } else {
+//             row += ' ';
+//         }
+//     }
+//     console.log(row);
+// }
+// }
 
-doSquare(16);
+// doSquare(16);
