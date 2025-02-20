dad35323cd7eb1a12c6888aa44b47b64677fc2f5
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-02-20 21:37:48 +0100

    Add tasks

---
+++ array.js
@@ -0,0 +1,91 @@
+// --------- Task 1 -----------
+
+let arr = [1, 5, 10, 89, 178];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// --------- Task 2 -----------
+
+arr = [2, 5, 10, 89, 178, 689]
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        console.log(arr[i]);
+    }
+}
+
+// --------- Task 3 -----------
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const resultArr = arr1.concat(arr2);
+
+// --------- Task 4 -----------
+
+const arr3 = [6, 5, 4];
+arr3.reverse();
+
+// --------- Task 5 -----------
+
+const arr4 = [1, 2, 3];
+arr4.push(4, 5, 6);
+
+// --------- Task 6 -----------
+
+const arr5 = [7, 8, 9];
+arr5.unshift(1, 2, 3)
+
+// --------- Task 7 -----------
+
+const arr6 = ['aaa', 'bbb', 'ccc'];
+console.log(arr6.shift());
+
+// --------- Task 8 -----------
+
+console.log(arr6.pop());
+
+// --------- Task 9 -----------
+
+const arr7 = [9, 10, 11, 12, 13];
+
+const newArray = arr7.slice(1);
+
+// --------- Task 10 -----------
+
+const arr8 = [1, 2, 3, 4, 5];
+arr8.splice(1, 2)
+
+// --------- Task 11 -----------
+
+const arr9 = [1, 2, 3, 4, 5];
+const arr10 = arr9.slice(2);
+
+// --------- Task 12 -----------
+
+const arr11 = [1, 2, 3, 4, 5];
+arr11.splice(1, 0,'w', 'trtr');
+arr11.pop();
+arr11.push('vvv', 'a', 'hello');
+
+// arr11.splice(1, arr11.length, 'w', 'trtr', 2, 3, 4, 'vvv', 'a', 'hello')
+
+// --------- Task 13* -----------
+
+const vovelsArray = ["a", "e", "i", "o", "u", "y"];
+
+function countVovels(str, vovelsArray) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (vovelsArray.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVovels('lorem ipsum dolor sit amet', vovelsArray));
+
